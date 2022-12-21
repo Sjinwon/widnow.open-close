87cf5f76d5c56ce2ec7ff749288a0897dff938c7
Shen jinwon <bosv031999@gmail.com>
2022-12-21 14:32:45 +0900

    2022-12-21

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let popup;
 
     function openPop(){
-        popup = window.open('http://127.0.0.1:5501/common/mainIndex.html',
+        popup = window.open('../html/mainIndex.html',
         'testPopup','width=1000px, height=800px, scrollbars=yes, top=100px, left=200px');
     }
 
